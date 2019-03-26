d49a5a4166e3ed0d70bb7e201e5d09e2aeaa79c5
Vitaliy Isikov <visikov@gmail.com>
2019-03-26 16:57:07 -0400

    Fix compilation target

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import webpack from 'webpack'
+import webpack, { Plugin } from 'webpack'
 
 import { apply } from './apply'
 
-export class HtmlInlineSourcePlugin extends webpack.Plugin {
+export class HtmlInlineSourcePlugin extends Plugin {
   apply(compiler: webpack.Compiler) {
     return apply(compiler)
   }
