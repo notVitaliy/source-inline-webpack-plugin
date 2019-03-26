025a47687df0b34f99827b769bbea920029265c4
Vitaliy Isikov <visikov@gmail.com>
2019-03-26 16:47:53 -0400

    Remove V4

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import webpack = require('webpack')
 
-import semver from 'semver'
-import { getHtmlWebpackPluginVersion } from './getHtmlWebpackPluginVersion'
-
-const webpackVersion = getHtmlWebpackPluginVersion()
-const isHtmlWebpackPlugin3 = semver.lt(webpackVersion, '4.0.0')
-
-const apply = isHtmlWebpackPlugin3 ? require('./apply-3').apply : require('./apply-4').apply
+import { apply } from './apply'
 
 export class HtmlInlineSourcePlugin extends webpack.Plugin {
   apply(compiler: webpack.Compiler) {
